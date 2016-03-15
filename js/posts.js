2edffd740f6a29e4912fc0f7c1338820b3b45490
Yeehan Chan <yihan.chen1994@gmail.com>
2016-03-14 21:14:36 -0400

    Don't even have time to set up a server

--- js/posts.js
+++ js/posts.js
@@ -1,11 +1,20 @@
 /* make the API call */
 
 var posts = ['lalala'];
+var nextpage;
 function getUserFeeds(response){
 	FB.api("/me/feed", function (response) {
-	      if (response && !response.error) {
-	        /* handle the result */
-	        posts = response.data;
+		      if (response && !response.error) {
+		      	/* handle the result */
+		        posts = response.data;
+		        while(posts.length > 0){
+		        	nextpage = response.paging.next;
+		        	FB.api(nextpage, function(response){
+		        		posts = posts.concat(response.data);
+		        	});
+		        }
+
+		      }
 	      }
 	    }
 	);
